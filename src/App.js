ec5f512c3bce3eb066fddec6f1dcfd934c21d1ea
Lucas Capart <lucas.capart80@gmail.com>
2023-07-07 18:09:10 +0200

    Update App.js
    Ajout du composant cursor à App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import Cursor from './components/Cursor/Cursor';
 
 const App = () => {
   return (
     <div>
+      <Cursor />
       <BrowserRouter>
         <Routes>
           <Route path='/home' element={<Home />} />
           <Route path='*' element={<Home />} />
         </Routes>
       </BrowserRouter>
+
     </div>
   );
 };
