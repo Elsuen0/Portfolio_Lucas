3dab9aecdf8a54916e2ffa47a400660a955dd58b
Lucas Capart <lucas.capart80@gmail.com>
2023-07-09 14:07:32 +0200

    Update Header.js
    Intégrations des boutons => Composant <Buttons /> & Liens de navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import './Header.scss'
-import BurgerMenu from '../BurgerMenu/BurgerMenu';
+import './Header.scss';
+import Buttons from '../SocialMedia_buttons/Buttons.js';
+
 
 const Header = () => {
     return (
-        <header>
-            <div className="headerIn">
-                <a href="/home">LUCAS</a>
-                <BurgerMenu />
-            </div>
-        </header>
+        <div className="headerIn">
+            <section className="links">
+                <a className='headerLink headerLink_about' href="/home">A Propos</a>
+                <a className='headerLink headerLink_works' href="#">Mes Projets</a>
+            </section>
+
+            <section className='sectionSocialMediaButtons'>
+                <Buttons />
+            </section>
+        </div>
     );
 };
 
