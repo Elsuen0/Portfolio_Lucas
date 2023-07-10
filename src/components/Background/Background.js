09b3eed0290ea5f11555c206861a00fc9a3baf74
Lucas Capart <lucas.capart80@gmail.com>
2023-07-10 16:27:15 +0200

    Create Background.js
    Création du composant background

---
+++ src/components/Background/Background.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Background.scss'
+
+const Background = () => {
+    return (
+        <div className='background'>
+            <p>DEV</p>
+            <p>WEB</p>
+        </div>
+    );
+};
+
+export default Background;
