536e69a4ba593b0d8b70cab6e2d1d306e4dc2501
Lucas Capart <lucas.capart80@gmail.com>
2023-07-10 16:25:25 +0200

    Update Home.js
    Modification des composants sur la page d'accueil

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
+import BurgerMenu from '../../components/BurgerMenu/BurgerMenu';
 import Me from '../../components/About/Me';
+import './Home.scss'
+import Background from '../../components/Background/Background';
 
 const Home = () => {
     return (
-        <div>
+        <div className='homeStyle'>
             <Header />
             <main>
-
+                <BurgerMenu />
+                <Me />
+                <Background />
             </main>
         </div>
     );
