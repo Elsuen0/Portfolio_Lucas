4d90c0561c500c52e599bd66f1a4031759c510fa
Lucas Capart <lucas.capart80@gmail.com>
2023-07-11 12:50:44 +0200

    Création composant + css About

---
+++ src/components/About/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './About.scss'
+
+const About = () => {
+    return (
+        <div className='aboutDescription'>
+            <h1>Je suis Lucas</h1>
+            <h2>Développeur Web</h2>
+            <p>
+                Fort d'une passion inébranlable pour le développement web depuis de nombreuses années, j'ai choisi d'en faire ma vocation. Doté d'une capacité d'apprentissage rapide et d'une compréhension intuitive, je m'efforce constamment de réaliser des exploits afin de maintenir un niveau d'efficacité optimal.
+            </p>
+        </div>
+    );
+};
+
+export default About;
