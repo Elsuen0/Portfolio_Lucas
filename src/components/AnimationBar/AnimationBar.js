2c76ac4327dfbe814c90c10ddc371b760ab220f8
Lucas Capart <lucas.capart80@gmail.com>
2023-07-11 14:59:23 +0200

    Create AnimationBar.js

---
+++ src/components/AnimationBar/AnimationBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AnimationBar.scss'
+
+const AnimationBar = () => {
+    return (
+        <div className='bar'>
+
+        </div>
+    );
+};
+
+export default AnimationBar;
