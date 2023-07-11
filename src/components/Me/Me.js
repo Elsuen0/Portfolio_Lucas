decad0d7355750bfcfca5369516111439e03d0ed
Lucas Capart <lucas.capart80@gmail.com>
2023-07-11 20:01:21 +0200

    Update Me.js

--- src/components/Me/Me.js
+++ src/components/Me/Me.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import './Me.scss'
+import Background from '../Background/Background';
+import Scroll from '../Scroll/Scroll';
 
 const Me = () => {
     return (
         <section className='mainSection_home'>
-            <div className='description_home'>
+            {/*<div className='description_home'>
                 <p className='paragraphe_haut'>Développeur web</p>
                 <p className='paragraphe_bas'>Prêt à transformer vos idées en sites web remarquables ?</p>
-            </div>
+    </div>*/}
 
             <div className='meSection'>
-                <h1>Bonjou<span className='style_R'>r</span>,</h1>
-                <h2>Je suis Lucas</h2>
-                <div className='meGroupButton'>
+                <h1>Bonjou<span className='style_R'>r</span>, je suis</h1>
+                <h2>Lucas Capart</h2>
+                <h3>Développeur<br /> FrontEnd</h3>
+                {/*<div className='meGroupButton'>
                     <button className='buttonMe'>Me Contacter</button>
-                </div>
+</div>*/}
             </div>
-
+            <Scroll />
         </section>
     );
 };
