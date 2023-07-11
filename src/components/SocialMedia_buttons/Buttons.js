94f2a67847c83ae77cfd7a2820991bd5b9fb7366
Lucas Capart <lucas.capart80@gmail.com>
2023-07-11 12:48:59 +0200

    Update Buttons.js
    Ajout des liens sur les boutons sociaux

--- src/components/SocialMedia_buttons/Buttons.js
+++ src/components/SocialMedia_buttons/Buttons.js
@@ -7,8 +7,8 @@ import './Buttons.scss'
 const Buttons = () => {
     return (
         <div className='socialMediaButtons'>
-            <button><FontAwesomeIcon icon={faGithub} /></button>
-            <button><FontAwesomeIcon icon={faLinkedin} /></button>
+            <a href="https://github.com/Elsuen0?tab=repositories" target='_blank' rel='noopener noreferrer'><button><FontAwesomeIcon icon={faGithub} /></button></a>
+            <a href="https://www.linkedin.com/in/lucas-capart/" target='_blank' rel='noopener noreferrer'><button><FontAwesomeIcon icon={faLinkedin} /></button></a>
             <button><FontAwesomeIcon icon={faEnvelope} /></button>
         </div>
     );
