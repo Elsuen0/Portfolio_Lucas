7913457bd04dcc8933882f7d5e942db8f1a608b0
Lucas Capart <lucas.capart80@gmail.com>
2023-07-11 20:01:13 +0200

    Create Scroll.js

---
+++ src/components/Scroll/Scroll.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Scroll.scss'
+
+const Scroll = () => {
+    return (
+        <p className='scroll'>
+            Scroll
+        </p>
+    );
+};
+
+export default Scroll;
