2880dfbc14dfdb88e2866ffac3fedf96ba2285d2
Lucas Capart <lucas.capart80@gmail.com>
2023-07-12 19:16:29 +0200

    Update About.js
    Ajout transition lors du scroll pour la partie 'About'

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,30 +1,46 @@
 import React, { useEffect } from 'react';
 import './About.scss'
+import { useSpring, animated } from 'react-spring';
+import { useInView } from 'react-intersection-observer';
+import Counter from '../CompteurPage/Counter';
 
 const About = () => {
+    const [ref, inView] = useInView({
+        triggerOnce: true,
+        threshold: 0.2, // Définir le seuil d'intersection ici selon vos besoins
+    });
+
+    const props = useSpring({
+        opacity: inView ? 1 : 0,
+        transform: inView ? 'translateY(0px)' : 'translateY(100px)',
+        config: { duration: 800 }, // Augmentez la durée de transition en millisecondes
+    });
     useEffect(() => {
         const cvButton = document.getElementById('cv-button');
         cvButton.addEventListener('click', () => {
             window.open('./CV_DW_Lucas_Capart.pdf', '_blank');
         });
-    })
+    });
+
     return (
-        <div className='aboutDescription' id='About'>
-            <h1>Je suis Lucas</h1>
-            <h2>Développeur Web</h2>
-            <p>
-                Salut ! Je suis Lucas, un développeur web passionné et créatif de 26 ans.
+        <div className='aboutDescription' id='About' ref={ref}>
+            <animated.div style={props}>
+                <h1>Je suis Lucas</h1>
+                <h2>Développeur Web</h2>
+                <p>
+                    Salut ! Je suis Lucas, un développeur web passionné et créatif de 26 ans.
 
-                Je suis constamment à la recherche de nouvelles opportunités pour créer des expériences en ligne <span className='style_R'>captivantes</span> et <span className='style_R'>innovantes</span>. Mon objectif est de transformer des idées en réalité en utilisant les dernières technologies web.<br />
+                    Je suis constamment à la recherche de nouvelles opportunités pour créer des expériences en ligne <span className='style_R'>captivantes</span> et <span className='style_R'>innovantes</span>. Mon objectif est de transformer des idées en réalité en utilisant les dernières technologies web.<br />
 
-                <br />Je suis <span className='style_R'>motivé</span> par les défis et les solutions innovantes. Je suis constamment à l'affût des dernières tendances du développement web et j'aime <span className='style_R'>collaborer</span> en équipe pour créer des produits de qualité. Je suis à l'aise dans un environnement agile qui encourage l'échange d'idées et la rétroaction.<br />
+                    <br />Je suis <span className='style_R'>motivé</span> par les défis et les solutions innovantes. Je suis constamment à l'affût des dernières tendances du développement web et j'aime <span className='style_R'>collaborer</span> en équipe pour créer des produits de qualité. Je suis à l'aise dans un environnement agile qui encourage l'échange d'idées et la rétroaction.<br />
 
-                <br />Si vous <span className='style_R'>recherchez</span> un développeur web passionné, polyvalent et désireux d'apprendre, je suis <span className='style_R'>prêt</span> à relever de nouveaux défis. Contactez-moi pour discuter de vos projets et découvrir comment nous pouvons créer quelque chose d'<span className='style_R'>extraordinaire</span> ensemble.
-            </p>
-            <div className="buttonSection">
-                <button className='buttonContact'>Me Contacter</button>
-                <button className='buttonCv' id='cv-button'>Voir Mon Cv</button>
-            </div>
+                    <br />Si vous <span className='style_R'>recherchez</span> un développeur web passionné, polyvalent et désireux d'apprendre, je suis <span className='style_R'>prêt</span> à relever de nouveaux défis. Contactez-moi pour discuter de vos projets et découvrir comment nous pouvons créer quelque chose d'<span className='style_R'>extraordinaire</span> ensemble.
+                </p>
+                <div className="buttonSection">
+                    <button className='buttonContact'>Me Contacter</button>
+                    <button className='buttonCv' id='cv-button'>Voir Mon Cv</button>
+                </div>
+            </animated.div>
         </div>
     );
 };
