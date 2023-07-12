9ad1cc97b017e341f78eb8bf9350cd855edce0c3
Lucas Capart <lucas.capart80@gmail.com>
2023-07-12 19:13:35 +0200

    Update Home.js
    Import du compteur de page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,22 +3,27 @@ import Header from '../../components/Header/Header';
 import BurgerMenu from '../../components/BurgerMenu/BurgerMenu';
 import Me from '../../components/Me/Me';
 import './Home.scss'
-import Background from '../../components/Background/Background';
 import About from '../../components/About/About';
 import AnimationBar from '../../components/AnimationBar/AnimationBar';
+import Counter from '../../components/CompteurPage/Counter';
 
 const Home = () => {
     return (
         <div className='homeStyle'>
             <Header />
-            <main>
+            <main id='main'>
                 <BurgerMenu />
                 <Me />
                 {/*<Background />*/}
+                <Counter numPage={'01'} />
             </main>
-            <section className='sousSection1'>
+            <section id='sousSection1'>
                 <AnimationBar />
                 <About />
+                <Counter numPage={'02'} />
+            </section>
+            <section id='sousSection2'>
+
             </section>
         </div>
     );
