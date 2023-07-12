ee490abb8fa408da6802973463566b24de389cfb
Lucas Capart <lucas.capart80@gmail.com>
2023-07-12 19:14:37 +0200

    Update Header.js
    Modification Href

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,8 +7,8 @@ const Header = () => {
     return (
         <div className="headerIn">
             <section className="links">
-                <a className='headerLink headerLink_about smooth-scroll' href="#About">A Propos</a>
-                <a className='headerLink headerLink_works' href="#">Mes Projets</a>
+                <a className='headerLink headerLink_about smooth-scroll' href="#sousSection1">A Propos</a>
+                <a className='headerLink headerLink_works' href="#sousSection2">Mes Projets</a>
             </section>
 
             <section className='sectionSocialMediaButtons'>
