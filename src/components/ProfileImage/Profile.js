ccf92d6b5ba1c94a86dff7228c6161dbc6fe94cc
Lucas Capart <lucas.capart80@gmail.com>
2023-07-19 15:06:55 +0200

    Create Profile.js
    Création du composant Profile

---
+++ src/components/ProfileImage/Profile.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Profile.scss'
+
+const Profile = () => {
+    return (
+        <div className="image-container">
+            <img src="./Elsueno.png" alt="Photo de profil El sueno" />
+            <div className='overlay-text'>
+                <h1>Pas de panique !</h1>
+                <p>
+                    Ce n'est pas vraiment moi ! Je vous présente : <span className='style_R'>El Sueño</span> ; l'antagoniste de la franchise <span className='style_R'>Ghost Recon</span>. Son pseudo me suit depuis plusieurs années maintenant et son image aussi, me permettant ainsi de garder <span className='style_R'>l'anonymat</span>. (Même si vous pouvez trouver mon CV juste à gauche)
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
