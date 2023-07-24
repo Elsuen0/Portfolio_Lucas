b8dae7407a54ef4a90b54125ceda0b26a0d746a2
Lucas Capart <lucas.capart80@gmail.com>
2023-07-24 16:46:54 +0200

    Create Carrousel.js
    Création du composant Carrousel

---
+++ src/components/Carrousel/Carrousel.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import './Carrousel.scss'
+
+const Carrousel = ({ projects }) => {
+
+    const settings = {
+        clasName: "center",
+        dots: true,
+        centerMode: true,
+        centerPadding: "25%",
+        infinite: true,
+        slidesToShow: 1,
+        slideToScroll: 1,
+        speed: 500,
+        focusOnSelect: true,
+    };
+
+    return (
+        <div>
+            <Slider {...settings}>
+                {projects.map((project) => (
+                    <div key={project.id} className='slide'>
+                        <img src={project.image} alt={project.title} />
+                        <h2>{project.title}</h2>
+                    </div>
+                ))}
+            </Slider>
+        </div>
+    );
+};
+
+export default Carrousel;
