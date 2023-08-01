cc31bea28f5daa1a13bd5f1d262a35cb3ac2deea
Lucas Capart <lucas.capart80@gmail.com>
2023-08-01 13:55:46 +0200

    Update Buttons.js
    Ajout des aria-labels

--- src/components/SocialMedia_buttons/Buttons.js
+++ src/components/SocialMedia_buttons/Buttons.js
@@ -7,9 +7,9 @@ import './Buttons.scss'
 const Buttons = () => {
     return (
         <div className='socialMediaButtons'>
-            <a href="https://github.com/Elsuen0?tab=repositories" target='_blank' rel='noopener noreferrer'><button><FontAwesomeIcon icon={faGithub} /></button></a>
-            <a href="https://www.linkedin.com/in/lucas-capart/" target='_blank' rel='noopener noreferrer'><button><FontAwesomeIcon icon={faLinkedin} /></button></a>
-            <a href="#footer"><button><FontAwesomeIcon icon={faEnvelope} /></button></a>
+            <a href="https://github.com/Elsuen0?tab=repositories" target='_blank' rel='noopener noreferrer' aria-label='Se rendre sur github' ><button title='Github'><FontAwesomeIcon icon={faGithub} /></button></a>
+            <a href="https://www.linkedin.com/in/lucas-capart/" target='_blank' rel='noopener noreferrer' aria-label='Se rendre sur linkedin'><button title='Linkedin'><FontAwesomeIcon icon={faLinkedin} /></button></a>
+            <a href="#footer" aria-label='Contacter Lucas'><button title='Contact'><FontAwesomeIcon icon={faEnvelope} /></button></a>
         </div>
     );
 };
