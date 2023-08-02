c4b7e8136a61755f05bf9b233194265280608604
Lucas Capart <lucas.capart80@gmail.com>
2023-08-02 12:41:48 +0200

    Update Skills.js
    Ajout <h3>

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Skills.scss'
 
-const Skills = ({ skillImage, percentage }) => {
+const Skills = ({ skillImage, percentage, title }) => {
     const radius = 45; // Rayon du cercle (ajustez selon vos besoins)
     const circumference = 2 * Math.PI * radius;
     const offset = circumference * (1 - percentage / 100);
@@ -11,17 +11,20 @@ const Skills = ({ skillImage, percentage }) => {
         strokeDashoffset: offset,
     };
     return (
-        <div className="skill-circle">
-            <svg className="circle-background">
-                <circle cx="50%" cy="50%" r={radius + '%'} />
-            </svg>
-            <svg className="circle-fill" style={circleStyle}>
-                <circle cx="50%" cy="50%" r={radius + '%'} />
-            </svg>
-            <img src={skillImage} alt="Skill" className='skill-image' />
-            <div className="overlay-skills">
-                <p>{percentage}%</p>
+        <div>
+            <div className="skill-circle">
+                <svg className="circle-background">
+                    <circle cx="50%" cy="50%" r={radius + '%'} />
+                </svg>
+                <svg className="circle-fill" style={circleStyle}>
+                    <circle cx="50%" cy="50%" r={radius + '%'} />
+                </svg>
+                <img src={skillImage} alt="Skill" className='skill-image' />
+                <div className="overlay-skills">
+                    <p>{percentage}%</p>
+                </div>
             </div>
+            <h3 className='titleSkill'>{title}</h3>
         </div>
     );
 };
